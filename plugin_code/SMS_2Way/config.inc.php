d83f4a44ef454c8e13de3a9eb281af29b060edf6
jigagile <jpatel@agileinfoways.com>
2013-07-09 11:17:26 +0530

    update public config file

---
+++ plugin_code/SMS_2Way/config.inc.php
@@ -0,0 +1,5 @@
+<?php 
+//defining file path for both devloper and product version.
+define("DEV",     "d/plugin_dev/plugin_code/SMS_2Way/");
+define("PROD",     "d/plugin_prod/plugin_code/SMS_2Way/");
+?>
